a38fc57f8e7cadf605701559d2c0da15a0680247
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-06-02 22:09:36 +0530

    Time: 55 ms (25.48%), Space: 56.3 MB (43.7%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,6 +1,30 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+  public int[] sortArray(int[] nums) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    for(int i : nums){
+        if(i < min){
+            min = i;
+        }
+        if(i > max){
+            max = i;
+        }
     }
+
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    int index = 0;
+    for (int num = min; num <= max; num++) {
+      while (map.containsKey(num) && map.get(num) > 0) {
+        nums[index] = num;
+        index++;
+        map.put(num, map.get(num) - 1);
+      }
+    }
+
+    return nums;
+  }
 }
