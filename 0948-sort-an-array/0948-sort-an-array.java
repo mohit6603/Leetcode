7a4fa348045677689700ba89f906afc7b376d398
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-06-03 11:52:35 +0530

    Time: 23 ms (77.27%), Space: 56.9 MB (20.51%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,29 +1,48 @@
 class Solution {
-  public int[] sortArray(int[] nums) {
-    int min = Integer.MAX_VALUE;
-    int max = Integer.MIN_VALUE;
-    for(int i : nums){
-        if(i < min){
-            min = i;
-        }
-        if(i > max){
-            max = i;
-        }
+    public int[] sortArray(int[] nums) {
+        int si = 0, ei = nums.length-1;
+        mergeSort(nums, si, ei);
+        return nums;
     }
 
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int num : nums) {
-      map.put(num, map.getOrDefault(num, 0) + 1);
+    public static void mergeSort(int[] nums, int si, int ei){
+        if(si >= ei){
+            return;
+        }
+            int mid = si+(ei-si)/2;
+            mergeSort(nums, si, mid);
+            mergeSort(nums, mid+1, ei);
+            merge(nums, si, mid, ei);
     }
 
-    int i = 0;
-    for (int num = min; num <= max; num++) {
-      while (map.containsKey(num) && map.get(num) != 0) {
-        nums[i] = num;
-        i++;
-        map.put(num, map.get(num) - 1);
-      }
+    public static void merge(int[] nums, int si, int mid, int ei){
+        int temp[] = new int[ei-si+1];
+        int i = si, j = mid+1, k = 0;
+
+        while(i <= mid && j <= ei){
+            if(nums[i] < nums[j]){
+                temp[k] = nums[i];
+                i++;
+            }
+            else{
+                temp[k] = nums[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i <= mid){
+            temp[k++] = nums[i++];
+        }
+        while(j <= ei){
+            temp[k++] = nums[j++];
+        }
+        // int m = 0;
+        // for(int n = 0; n < temp.length; n++){
+        //     nums[m++] = temp[n];
+        // }
+
+        System.arraycopy(temp, 0, nums, si, temp.length);
+        
     }
-    return nums;
-  }
 }
