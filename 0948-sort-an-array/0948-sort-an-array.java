ff73e34c9d22e8abf3688dc405805133ffa373c8
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-06-04 22:32:05 +0530

    Time: 89 ms (17.37%), Space: 58.1 MB (8.42%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,48 +1,29 @@
-class Solution {
-    public int[] sortArray(int[] nums) {
-        int si = 0, ei = nums.length-1;
-        mergeSort(nums, si, ei);
-        return nums;
-    }
+// class Solution {
+//     public int[] sortArray(int[] nums) {
 
-    public static void mergeSort(int[] nums, int si, int ei){
-        if(si >= ei){
-            return;
-        }
-            int mid = si+(ei-si)/2;
-            mergeSort(nums, si, mid);
-            mergeSort(nums, mid+1, ei);
-            merge(nums, si, mid, ei);
-    }
+//         int i=0;
+//         int[] ans = new int[nums.length];
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         for(int num : nums) pq.offer(num);
+//         while(!pq.isEmpty()) ans[i++] = pq.poll();
+//         return ans;
+//     }
+// }
 
-    public static void merge(int[] nums, int si, int mid, int ei){
-        int temp[] = new int[ei-si+1];
-        int i = si, j = mid+1, k = 0;
+class Solution{
+    public int[] sortArray(int[] nums){
+        int i = 0;
+        int ans[] = new int [nums.length];
+        Queue<Integer> pq = new PriorityQueue<>();
 
-        while(i <= mid && j <= ei){
-            if(nums[i] < nums[j]){
-                temp[k] = nums[i];
-                i++;
-            }
-            else{
-                temp[k] = nums[j];
-                j++;
-            }
-            k++;
+        for(int j = 0; j< nums.length; j++){
+            pq.offer(nums[j]);
         }
 
-        while(i <= mid){
-            temp[k++] = nums[i++];
+        while(!pq.isEmpty()){
+            ans[i++] = pq.poll();
         }
-        while(j <= ei){
-            temp[k++] = nums[j++];
-        }
-        // int m = 0;
-        // for(int n = 0; n < temp.length; n++){
-        //     nums[m++] = temp[n];
-        // }
 
-        System.arraycopy(temp, 0, nums, si, temp.length);
-        
+        return ans;
     }
 }
