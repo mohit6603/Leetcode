d8c6f8dab65059458617d08e6c83ed4653394c03
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-06-04 22:33:01 +0530

    Time: 90 ms (16.85%), Space: 55.2 MB (90.58%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,15 +1,3 @@
-// class Solution {
-//     public int[] sortArray(int[] nums) {
-
-//         int i=0;
-//         int[] ans = new int[nums.length];
-//         PriorityQueue<Integer> pq = new PriorityQueue<>();
-//         for(int num : nums) pq.offer(num);
-//         while(!pq.isEmpty()) ans[i++] = pq.poll();
-//         return ans;
-//     }
-// }
-
 class Solution{
     public int[] sortArray(int[] nums){
         int i = 0;
