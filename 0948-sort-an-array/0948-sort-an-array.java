a5470f17a6a5cdd0f9ba52cfb833fbc49b2c631b
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-06-13 08:47:36 +0530

    Time: 23 ms (77.79%), Space: 56.9 MB (20.64%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,17 +1,48 @@
-class Solution{
-    public int[] sortArray(int[] nums){
-        int i = 0;
-        int ans[] = new int [nums.length];
-        Queue<Integer> pq = new PriorityQueue<>();
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int si = 0, ei = nums.length-1;
+        mergeSort(nums, si, ei);
+        return nums;
+    }
 
-        for(int j = 0; j< nums.length; j++){
-            pq.offer(nums[j]);
+    public static void mergeSort(int[] nums, int si, int ei){
+        if(si >= ei){
+            return;
+        }
+            int mid = si+(ei-si)/2;
+            mergeSort(nums, si, mid);
+            mergeSort(nums, mid+1, ei);
+            merge(nums, si, mid, ei);
+    }
+
+    public static void merge(int[] nums, int si, int mid, int ei){
+        int temp[] = new int[ei-si+1];
+        int i = si, j = mid+1, k = 0;
+
+        while(i <= mid && j <= ei){
+            if(nums[i] < nums[j]){
+                temp[k] = nums[i];
+                i++;
+            }
+            else{
+                temp[k] = nums[j];
+                j++;
+            }
+            k++;
         }
 
-        while(!pq.isEmpty()){
-            ans[i++] = pq.poll();
+        while(i <= mid){
+            temp[k++] = nums[i++];
         }
+        while(j <= ei){
+            temp[k++] = nums[j++];
+        }
+        // int m = 0;
+        // for(int n = 0; n < temp.length; n++){
+        //     nums[m++] = temp[n];
+        // }
 
-        return ans;
+        System.arraycopy(temp, 0, nums, si, temp.length);
+        
     }
 }
