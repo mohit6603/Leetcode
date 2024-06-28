b9f22bc2a25957eeb5f7ee6017ee8dc72527bd1c
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-06-28 19:16:45 +0530

    Time: 0 ms (100%), Space: 40.1 MB (52.9%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int [n+1];
+
+        return helper(n, dp);
+    }
+
+    public int helper(int n, int dp[]) {
+        if(n == 1 || n == 2){
+            dp[n] = n;
+            return dp[n];
+        }
+
+        if(dp[n] != 0){
+            return dp[n];
+        }
+
+        dp[n] = helper(n-1, dp) + helper(n-2, dp);
+        return dp[n];
+    }
+
+}
