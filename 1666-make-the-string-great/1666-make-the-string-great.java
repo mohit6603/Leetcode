a7d5b4abe70fbef9e85a53498696b901bf2df47d
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-06-28 10:24:38 +0530

    Time: 3 ms (58.56%), Space: 42.1 MB (50.29%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.java
+++ 1666-make-the-string-great/1666-make-the-string-great.java
@@ -1,7 +1,6 @@
 class Solution {
     public String makeGood(String s) {
         if(s.length() == 0 || s.length() == 1) return s;
-
         Stack<Character> st = new Stack<>();
         for(int i = 0; i<s.length(); i++){
             char ch = s.charAt(i);
@@ -9,11 +8,10 @@ class Solution {
             else if(ch+32 == st.peek() || ch-32 == st.peek()) st.pop();
             else st.push(ch);
         }
-
         s = "";
         while(!st.isEmpty()){
-            s  = st.pop() + s;
-        } 
+            s = st.pop() + s;
+        }
         return s;
     }
 }
