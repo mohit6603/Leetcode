271398583824a368ab6a3e574f92f35d14cc90e4
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-09-03 07:00:30 +0530

    Time: 32 ms (5.11%), Space: 58.5 MB (33.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,8 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1] ) return true;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){
+            if(map.containsKey(i)){
+                map.put(i, map.get(i)+1);
+            }
+            else{
+                map.put(i, 1);
+            }
+        }
+
+        for(Integer j : map.keySet()){
+            if(map.get(j) > 1) return true;
         }
         return false;
     }
