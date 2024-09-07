6c1dff9897225040906deba8150313a1ddcc9ebf
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-09-07 21:00:21 +0530

    Time: 0 ms (100%), Space: 40.9 MB (26.84%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,14 +1,10 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0){
-            return false;
-        }
-        else if(n == 1){
-            return true;
-        }
-        else if(n%2 == 0){
+        if(n <= 0) return false;
+        else if (n == 1) return true;
+        else if (n%2 == 0){
             return isPowerOfTwo(n/2);
         }
-        return false;
+        else return false;
     }
 }
