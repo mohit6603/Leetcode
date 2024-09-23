88109fbf0fd24e0083a06000df108523cbe72d97
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-09-23 19:38:59 +0530

    Time: 1 ms (98.91%), Space: 43.7 MB (39.07%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,14 +1,13 @@
 class Solution {
-    public int[] finalPrices(int[] prices) {
-        for(int i = 0; i<prices.length-1; i++){
-            int dis = prices[i];
-            for(int j = i+1; j<prices.length; j++){
-                if(prices[j] <= dis){
-                    prices[i] = prices[i]-prices[j];
+    public int[] finalPrices(int[] nums) {
+        for(int i = 0; i < nums.length-1; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[j] <= nums[i]){
+                    nums[i] = nums[i]-nums[j];
                     break;
                 }
             }
         }
-        return prices;
+        return nums;
     }
 }
