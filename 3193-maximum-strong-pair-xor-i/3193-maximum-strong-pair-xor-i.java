c6736248596319827582d8699c4538c952455a50
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-09-25 06:43:37 +0530

    Time: 4 ms (20.14%), Space: 43 MB (49.37%) - LeetHub

--- 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.java
+++ 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.java
@@ -1,17 +1,19 @@
 class Solution {
     public int maximumStrongPairXor(int[] nums) {
+        
         int xor = 0;
         for(int i = 0; i<nums.length; i++){
             for(int j = 0; j<nums.length; j++){
-                if(Math.abs(nums[i] - nums[j]) <= Math.min(nums[i], nums[j])){
-                    int xr = nums[i]^nums[j];
+                if(Math.abs(nums[i]-nums[j]) <= Math.min(nums[i], nums[j])){
+                    int newxor = nums[i]^nums[j];
 
-                    if(xr >= xor){
-                        xor = xr;
+                    if(newxor > xor){
+                        xor = newxor;
                     }
                 }
             }
         }
+
         return xor;
     }
 }
