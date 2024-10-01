6d0fdcb422096e7251372c61029df3144a2c1f0a
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-01 06:07:02 +0530

    Time: 3 ms (93.12%), Space: 43.5 MB (64.76%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int ans[] = new int [nums1.length];
-        Arrays.fill(ans, -1);
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
 
-        for(int i = 0; i < nums1.length; i++){
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j] && j != nums2.length-1){
-                    if(nums2[j+1] > nums2[j]){
-                        ans[i] = nums2[j+1];
-                        break;
-                    }
-                    else{
-                        for(int k = j+2; k < nums2.length; k++){
-                            if(nums2[k] > nums2[j]){
-                                ans[i] = nums2[k];
-                                break;
-                            }
-                        }
-                    }
-                }
+        //saare next greater find and store in n2
+        for(int i : nums2){
+            while(!st.isEmpty() && st.peek() < i){
+                map.put(st.pop(), i);
             }
+            st.push(i);
+        }
+
+        //fill ans arr
+        int ans[] = new int [nums1.length];
+        for(int i = 0; i< nums1.length; i++){
+            ans[i] = map.getOrDefault(nums1[i], -1);
         }
 
         return ans;
     }
 }
+
+//tc = o(m+n)
