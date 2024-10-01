437b562630c88871976bb98dd24d8aad1e3c179a
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-01 07:21:35 +0530

    Time: 6 ms (55.33%), Space: 53.5 MB (77.42%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int ans[] = new int[nums.length];
-        Arrays.fill(ans, 0);
         List<Integer> list = new ArrayList<>();
 
-        for(int i = 0; i<nums.length; i++){
-            int k = nums[i];
-            if(ans[k-1] == 0){
-                ans[k-1] = nums[i];
-            }
+        for(int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]) - 1;
+            nums[index] = -Math.abs(nums[index]);
         }
 
-        for(int i = 0; i<ans.length; i++){
-            if(ans[i] == 0){
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
                 list.add(i+1);
             }
         }
         return list;
     }
 }
+
+// o(n+n) no extra spacce
