b064034009bde259670f9832559a8c06ce0d5e5e
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-01 05:46:38 +0530

    Time: 4 ms (43.96%), Space: 43 MB (90.58%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int arr[] = new int[nums1.length];
-        Arrays.fill(arr, -1);
+        int ans[] = new int [nums1.length];
+        Arrays.fill(ans, -1);
 
-        for(int i = 0; i<nums1.length; i++){
-            for(int j = 0; j<nums2.length; j++){
+        for(int i = 0; i < nums1.length; i++){
+            for(int j = 0; j < nums2.length; j++){
                 if(nums1[i] == nums2[j] && j != nums2.length-1){
                     if(nums2[j+1] > nums2[j]){
-                        arr[i] = nums2[j+1];
+                        ans[i] = nums2[j+1];
                         break;
-                    }else if(nums2[j+1] <= nums2[j]){
-                        for(int n = j+2; n<nums2.length; n++){
-                        if(nums2[n] > nums1[i]){
-                            arr[i] = nums2[n];
-                            break;
-                        }
                     }
+                    else{
+                        for(int k = j+2; k < nums2.length; k++){
+                            if(nums2[k] > nums2[j]){
+                                ans[i] = nums2[k];
+                                break;
+                            }
+                        }
                     }
                 }
             }
         }
-        return arr;
+
+        return ans;
     }
 }
