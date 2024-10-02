50715ec5b6b9b31057e0bcc66bbc0b37cd28772f
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-03 05:24:33 +0530

    Time: 1 ms (98.21%), Space: 58.9 MB (80.91%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,41 +1,16 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean ans = false;
         if(nums[0] < nums[nums.length-1]){
-            ans = increasing(nums);
-        }else if(nums[0] > nums[nums.length-1]){
-            ans = decreasing(nums);
-        }
-        else if (nums[0] == nums[nums.length-1]){
-            ans = consta(nums);
-        }
-        return ans;
-    }
-
-    public boolean increasing(int nums[]){
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] > nums[i+1]){
-                return false;
+            for(int i = 0; i < nums.length-1; i++){
+                if(nums[i] > nums[i+1]) return false;
             }
         }
-        return true;
-    }
-
-    public boolean decreasing(int nums[]){
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] < nums[i+1]){
-                return false;
+        else{
+            for(int i = 0; i < nums.length-1; i++){
+                if(nums[i] < nums[i+1]) return false;
             }
         }
-        return true;
-    }
 
-    public boolean consta(int nums[]){
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] != nums[i+1]){
-                return false;
-            }
-        }
         return true;
     }
 }
