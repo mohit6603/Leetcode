42c5961f616c96b58cc72c4ca8b144c8bfe528b7
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-05 07:02:33 +0530

    Time: 2 ms (99.19%), Space: 42.9 MB (76.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,58 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int lp = 0;
-        int rp = s.length()-1;
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         if(s == null)return false;
+//         s = s.toLowerCase();
 
-        while(lp < rp){
-            if(s.charAt(lp) != s.charAt(rp)){
-                return false;
-            }
-            lp++;
-            rp--;
+//         int lp = 0;
+//         int rp = s.length()-1;
+
+//         while(lp < rp){
+//             if(!isAlphanumeric(s.charAt(lp))){
+//                 lp++;
+//             }
+//             else if(!isAlphanumeric(s.charAt(rp))){
+//                 rp--;
+//             }
+//             else if(s.charAt(lp) != s.charAt(rp)){
+//                 return false;
+//             }
+//             else{
+//                 lp++;
+//                 rp--;
+//             }
+//         }
+//     }
+// }
+
+class Solution {
+  public boolean isPalindrome(String s) {
+    if (s == null) {
+        return false;
+    }
+    int left = 0;
+    int right = s.length() - 1;
+    while (left < right) {
+        if (!isAlphanumeric(s.charAt(left))) {
+            left++;
+        } else if (!isAlphanumeric(s.charAt(right))) {
+            right--;
+        } else if (lowerCase(s.charAt(left)) != lowerCase(s.charAt(right)))  {
+            return false;
+        } else {
+            left++;
+            right--;
         }
-        return true;
+    }
+    return true;
+    }
+
+    private boolean isAlphanumeric(char c) {
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9');
+}
+    
+private char lowerCase(char c){
+      if('a' <= c && c <= 'z' ||  ('0' <= c && c <= '9')) return c;
+       
+        return (char)((int)c+32);
     }
 }
