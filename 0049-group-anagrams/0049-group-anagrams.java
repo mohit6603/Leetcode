16943fc76dde2e3700a812228b4a82beff991fe4
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-06 10:42:35 +0530

    Time: 5 ms (99.7%), Space: 47.5 MB (81.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        // if(strs = null || strs.length == 0) return new ArrayList<>();
+
         Map<String, List<String>> map = new HashMap<>();
 
-        for(String word : strs){
-
-            char ch[] = word.toCharArray();
-            Arrays.sort(ch);
-            String sortedWord = new String(ch);
-
-            if(!map.containsKey(sortedWord)){
-                map.put(sortedWord, new ArrayList<>());
+        for(String s : strs){
+            char ch[] = new char [26];
+            for(char c : s.toCharArray()){
+                ch[c - 'a']++;
             }
 
-            map.get(sortedWord).add(word);
+            String keyStr = String.valueOf(ch);
 
+            if(!map.containsKey(keyStr)){
+                map.put(keyStr, new ArrayList<>());
+            }
+            map.get(keyStr).add(s);
         }
+
         return new ArrayList<>(map.values());
     }
 }
 
-// tc = 0(n. klogk)
-// space = 0(n.k)
+// time = 0(n.k)
+// o(m+n)
