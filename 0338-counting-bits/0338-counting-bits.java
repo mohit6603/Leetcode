d9a3fb3e197e547c649efaff4d5f646a5fda46a3
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-07 05:36:12 +0530

    Time: 3 ms (41.71%), Space: 46.6 MB (36.34%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,21 @@
 class Solution {
     public int[] countBits(int n) {
         int ans[] = new int[n+1];
+        int count = 0;
+
         for(int i = 0; i <= n; i++){
-            ans[i] = Integer.bitCount(i);
+            ans[i] = bitcount(i);
         }
+
         return ans;
+    }
+
+    public int bitcount(int i){
+        int count = 0;
+        while(i  > 0){
+            count += i&1;
+            i >>= 1;
+        }
+        return count;
     }
 }
