f4ec707b081025a880fdaa1ddbbb1397029dad32
Mohit Patle <142034520+mohit6603@users.noreply.github.com>
2024-10-15 04:44:16 +0530

    Time: 27 ms (85.41%), Space: 51.1 MB (93.01%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> mainlist = new ArrayList<>();
+        List <List <Integer>> mainlist = new ArrayList<>();
         int n = nums.length;
         Arrays.sort(nums);
 
-        for(int  i = 0; i < n; i++){
-            if( i > 0 && nums[i] == nums[i-1]){
+        for(int i = 0; i < n; i++){
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
-
-            int n1 = nums[i];
-            int target = -n1;
             int lp = i+1;
             int rp = n-1;
+            int n1 = nums[i];
+            int target = -n1;
 
             while(lp < rp){
-                if(nums[lp] + nums[rp] > target) rp--;
-                else if(nums[lp]+nums[rp] < target) lp++;
-                else {
-                    while(lp < rp && nums[lp] == nums[lp+1]) lp++;
-                    while(lp < rp && nums[rp] == nums[rp-1]) rp--;
+                if(nums[lp]+nums[rp] > target){
+                    rp--;
+                }
+                else if(nums[lp] + nums[rp] < target){
+                    lp++;
+                }
+                else{
+                    while(lp < rp && nums[lp] == nums[lp+1])lp++;
+                    while(lp < rp && nums[rp] == nums[rp-1])rp--;
+
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[lp]);
                     list.add(nums[rp]);
@@ -30,6 +34,7 @@ class Solution {
                 }
             }
         }
+
         return mainlist;
     }
 }
